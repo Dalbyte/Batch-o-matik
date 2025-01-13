f1b6f5db7519c3a2c7ddf0e2f5a0456ab434dc00
Dalbyte <dalbert.alexander@gmail.com>
2025-01-13 18:37:58 +0100

    t

---
+++ video/VideoToGifBetterColors_600.bat
@@ -0,0 +1,13 @@
+@echo off
+:again
+cd /d %~dp1
+if "%~1" == "" goto done
+
+ffmpeg -v warning -i "%~n1%~x1"  -vf "fps=8,scale=400:-1:flags=lanczos,palettegen" -y "%~n1-color.png"
+ffmpeg -v warning -i "%~n1%~x1"  -i "%~n1-color.png" -lavfi "scale=400:-1:flags=lanczos [x]; [x][1:v] paletteuse" -y "%~n1-400_small.gif"
+del /q "%~n1-color.png"
+
+shift
+goto again
+:done
+exit
