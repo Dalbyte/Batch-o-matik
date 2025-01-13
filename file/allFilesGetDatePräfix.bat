2e5ed932b2c5f5d5543eff0e15de0e681c744696
Dalbyte <dalbert.alexander@gmail.com>
2025-01-13 18:35:42 +0100

    red

--- file/allFilesGetDatePräfix.bat
+++ file/allFilesGetDatePräfix.bat
@@ -3,14 +3,16 @@
 :again
 cd /d %~dp1
 if "%~1" == "" goto done
-    set creationDate=%~t1
-    rem echo %creationDate%
-    set creationDate=%creationDate:~6,4%%creationDate:~3,2%%creationDate:~0,2%
-    rem echo %creationDate%
+
+    for /f "tokens=* usebackq" %%i in (`powershell -command "(Get-Item '%~1').CreationTime.ToString('yyyyMMdd')"`) do set creationDate=%%i
+
+    echo %creationDate%
     rem echo %~n1%~x1
     ren "%~n1%~x1" "%creationDate%_%~n1%~x1"
+
 shift
 goto again
+
 :done
-rem pause
+pause
 exit
--- video/VideoToSmall_h264.bat
+++ video/VideoToSmall_h264.bat
@@ -3,7 +3,7 @@
 cd /d %~dp1
 if "%~1" == "" goto done
 
-ffmpeg -i "%~n1%~x1" -vf format=yuv420p -codec:v libx264 -crf 34 "%~n1_h264_small.mp4"
+ffmpeg -i "%~n1%~x1" -vf format=yuv420p -codec:v libx264 -crf 24 "%~n1_h264_small.mp4"
 
 
 shift
